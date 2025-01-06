9edb0795d63d44a5c85c9fdb8dade330b6e25bc7
Moulick Aggarwal <Moulick@users.noreply.github.com>
2025-01-06 16:35:24 +0530

    Add GitHub Clickable Links script

---
+++ github-clickable.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Make GitHub PR and Issue Titles Clickable
+// @namespace    https://github.com/Moulick
+// @version      0.2
+// @description  Make GitHub PR and Issue Titles Clickable
+// @author       Moulick Aggarwal
+// @supportURL   https://github.com/Moulick/userscripts/issues
+// @match        https://github.com/*
+// @run-at       document-start
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
+
+(function () {
+  'use strict';
+
+  function init() {
+    document.querySelector('.js-issue-title')?.addEventListener('click', () => {
+      document.querySelector('[aria-label="Edit Pull Request title"]')?.click()
+      document.querySelector('[aria-label="Edit Issue title"]')?.click()
+    })
+  }
+
+  document.addEventListener('DOMContentLoaded', init)
+  document.addEventListener('turbo:render', init)
+
+})();
