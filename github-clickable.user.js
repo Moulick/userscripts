94267ae2a586c19431bbe280efc551c4a03c0943
Moulick Aggarwal <Moulick@users.noreply.github.com>
2026-05-02 00:02:21 +0200

    Fix GitHub title edit selectors
    Update the userscript to match current GitHub issue and pull request title/edit controls.

--- github-clickable.user.js
+++ github-clickable.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Make GitHub PR and Issue Titles Clickable
 // @namespace    https://github.com/Moulick
-// @version      0.2
+// @version      0.3
 // @description  Make GitHub PR and Issue Titles Clickable
 // @author       Moulick Aggarwal
 // @supportURL   https://github.com/Moulick/userscripts/issues
-// @match        https://github.com/*
+// @match        https://github.com/*/*/issues/*
+// @match        https://github.com/*/*/pull/*
 // @run-at       document-start
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        none
@@ -16,14 +17,11 @@
 (function () {
   'use strict';
 
-  function init() {
-    document.querySelector('.js-issue-title')?.addEventListener('click', () => {
-      document.querySelector('[aria-label="Edit Pull Request title"]')?.click()
-      document.querySelector('[aria-label="Edit Issue title"]')?.click()
-    })
-  }
-
-  document.addEventListener('DOMContentLoaded', init)
-  document.addEventListener('turbo:render', init)
-
+  document.addEventListener('click', (event) => {
+    const title = event.target.closest('h1[data-component="PH_Title"] .markdown-title')
+    if (title) {
+      document.querySelector('[data-testid="edit-issue-title-button"]')?.click()
+      title.closest('h1[data-component="PH_Title"]')?.querySelector('button')?.click()
+    }
+  }, true)
 })();
